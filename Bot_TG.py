35b23d128b4c8b1176e10d2f3425d73944ab7609
Zablokirovan <artemiszbobylev05@gmail.com>
2026-03-23 17:30:17 +0500

    Created a function for calendar and note recording

--- Bot_TG.py
+++ Bot_TG.py
@@ -1,10 +1,15 @@
 import asyncio
 import os
-import utilities
+
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, CallbackQuery
+from aiogram_calendar import SimpleCalendar, SimpleCalendarCallback
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+import utilities
 
 load_dotenv()
 
@@ -14,8 +19,13 @@ dp = Dispatcher()
 
 button = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Погода")],
-    [KeyboardButton(text="Деньги")]
+    [KeyboardButton(text="Деньги")],
+    [KeyboardButton(text="Календарь")]
 ],resize_keyboard=True)
+
+
+class ReminderState(StatesGroup):
+    waiting_for_note = State()
 
 
 @dp.message(CommandStart())
@@ -57,6 +67,48 @@ async def money(message: Message):
                          f"🇺🇸(USD) - {data["USD"]}₸\n"
                          f"🇪🇺(EUR) - {data["EUR"]}₸\n"
                          f"🇷🇺(RUB) -{data["RUB"]}₸")
+
+# Для работы календаря
+@dp.message(F.text.lower() == "календарь")
+async def cmd_calendar(message: Message):
+    await message.answer(
+        "📅 Выбери дату:",
+        reply_markup=await SimpleCalendar().start_calendar()
+    )
+
+
+@dp.callback_query(SimpleCalendarCallback.filter())
+async def process_calendar(
+        callback: CallbackQuery,
+        callback_data: SimpleCalendarCallback,
+        state: FSMContext):
+
+    selected, date = await SimpleCalendar().process_selection(callback, callback_data)
+
+    await state.update_data(selected_date=date.strftime("%d.%m.%Y"))
+    await state.set_state(ReminderState.waiting_for_note)
+
+    await callback.message.answer(
+        f"✅ Дата выбрана: {date.strftime('%d.%m.%Y')}\n"
+        f"Теперь введи, что нужно запланировать на этот день:"
+    )
+
+    await callback.answer()
+
+
+@dp.message(ReminderState.waiting_for_note)
+async def save_note(message: Message, state: FSMContext):
+    user_data = await state.get_data()
+    selected_date = user_data.get("selected_date")
+    note_text = message.text
+
+    await message.answer(
+        f"📝 Заметка сохранена:\n"
+        f"Дата: {selected_date}\n"
+        f"Дело: {note_text}"
+    )
+
+    await state.clear()
 
 
 async def main():
